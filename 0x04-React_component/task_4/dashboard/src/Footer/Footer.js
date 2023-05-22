cb3803f8babd14e7e222aa010d76a4c137e47579
Alejandro Uran <alejandrourn@gmail.com>
2023-05-22 13:47:48 -0500

    update four task

--- 0x04-React_component/task_4/dashboard/src/Footer/Footer.js
+++ 0x04-React_component/task_4/dashboard/src/Footer/Footer.js
@@ -1,11 +1,13 @@
-import  React from 'react';
-import './Footer.css';
-import { getFullYear, getFooterCopy } from '../utils/utils';
+import React from "react";
+import "./Footer.css";
+import { getFullYear, getFooterCopy } from "../utils/utils";
 
 function Footer() {
   return (
     <footer className="Footer">
-			<i>Copyright {getFullYear()} - {getFooterCopy()}</i>
+      <i>
+        Copyright {getFullYear()} - {getFooterCopy()}
+      </i>
     </footer>
   );
 }
