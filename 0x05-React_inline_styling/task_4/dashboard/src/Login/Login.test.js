50344cd456d1313dd574e80ef49daf2e3307b1e1
Alejandro Uran <alejandrourn@gmail.com>
2023-05-25 22:08:54 -0500

    three four tasks react inline

---
+++ 0x05-React_inline_styling/task_4/dashboard/src/Login/Login.test.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+import Login from "./Login";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+describe("Basic React Tests - <Login />", function () {
+  it("Should render without crashing", () => {
+    const wrapper = shallow(<Login />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
---
+++ 0x05-React_inline_styling/task_4/dashboard/src/index.js
@@ -0,0 +1,13 @@
+import App from './App/App';
+import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
