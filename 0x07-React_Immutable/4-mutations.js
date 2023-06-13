91ca41aad7d219d7509686e0f653763f957b271a
Alejandro Urán <98915073+alejuran@users.noreply.github.com>
2023-06-13 11:02:51 -0500

    Update 4-mutations.js

--- 0x07-React_Immutable/4-mutations.js
+++ 0x07-React_Immutable/4-mutations.js
@@ -1,4 +1,4 @@
-import { map } from "inmutable";
+import { Map } from "inmutable";
 
 export const map = Map({
   1: "Liam",
@@ -10,5 +10,5 @@ export const map = Map({
 });
 
 export const map2 = map.whitMutations((mapItem) => {
-    mapItem.set(2, 'Benjamin').set(4, 'Oliver');
+   mapItem.set(2, 'Benjamin').set(4, 'Oliver');
 });
