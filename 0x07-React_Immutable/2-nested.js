d6e0fbe0180ca101b5c7d7f0ec78dc59bdf25853
Alejandro Uran <alejandrourn@gmail.com>
2023-06-13 10:28:40 -0500

    update nested inmutable

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,8 +1,8 @@
 import { fromJS } from "immutable";
 
 const accessInmutatableOject = (object, array) => {
-    const mapObj = fromJS(object);
-    return mapObj.getIn(array, undefined);
+  const mapObj = fromJS(object);
+  return mapObj.getIn(array, undefined);
 };
 
 export default accessInmutatableOject;
